1e215a5e59a6d3cce574b93dd31a51c10c93840a
Nathan Kerr <nathan@nathankerr.com>
2011-11-17 16:25:44 +0100

    Render now produces pdfs from tex output

--- src/render/render.go
+++ src/render/render.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"exec"
+	"path/filepath"
+	"io/ioutil"
 	"log"
+	"os"
 	"soar"
 )
 
@@ -13,17 +17,42 @@ func (r *Render) Render(filename string) []byte {
 		panic(err)
 	}
 
+	tmpdir, err := ioutil.TempDir("tmp/", "")
+	if err != nil {
+		panic(err)
+	}
+
 	returns, err := assets_consumer.Invoke("Get", filename)
 	if err != nil {
 		panic(err)
 	}
 
-	data, ok := returns[0].([]byte)
+	input, ok := returns[0].([]byte)
 	if !ok {
 		panic("could not recast assets return")
 	}
 
-	return data
+	input_file := filepath.Join(tmpdir, "input.tex")
+	ioutil.WriteFile(input_file, input, 0666)
+
+	pdf_out := filepath.Join(tmpdir, "input.pdf")
+	xelatex := exec.Command("xelatex",
+		//"-o", pdf_out,
+		"-output-directory=" + tmpdir,
+		input_file,
+	)
+	xelatex.Stdout = os.Stdout
+	xelatex.Stderr = os.Stderr
+	if xelatex.Run() != nil {
+		log.Fatal("pdfserver xelatex.Run:", err)
+	}
+
+	content, err := ioutil.ReadFile(pdf_out)
+	if err != nil {
+		log.Fatal("pdfserver read content:", err)
+	}
+
+	return content
 }
 
 func main() {
